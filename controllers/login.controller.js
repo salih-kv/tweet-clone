f0c01fce410807fb3d41f6e80f18c733dc22ba58
linson_kw <linson.lorance@kilowott.com>
2023-08-25 23:46:33 +0530

    fixed merge changes

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,13 +1,15 @@
 import bcrypt from "bcrypt";
 import User from "../model/user.model.js";
+import createToken from "./tokenGen.js";
 
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
   try {
     const validUser = await User.findOne({ username: username });
+    const maxAge =3*24*60*60;
 
-    if (!validUser)
-      return res.status(200).json({
+    if (!validUser)//error codes changed for unauthorised user
+      return res.status(401).json({
         errorcode: 1,
         status: false,
         message: "user not found",
@@ -15,14 +17,16 @@ export const loginUser = async (req, res) => {
       });
     let compare = await bcrypt.compare(password, validUser.password);
     if (!compare)
-      return res.status(200).json({
+      return res.status(401).json({
         errorcode: 2,
         status: false,
         message: "password not matching",
         data: null,
       });
-
-    return res.status(200).json({
+      
+    const token=createToken(validUser._id);  
+    res.cookie('jwt',token,{httpOnly:true,maxAge:maxAge*1000}); 
+    res.status(200).json({
       errorcode: 0,
       status: true,
       message: "login successfully",
--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,25 +1,22 @@
 import bcrypt from "bcrypt";
-import User from "../model/user.model.js";
+import formData from "../model/user.model.js";
+
 
 export const createUser = async (req, res) => {
   const { fname, lname, username, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-<<<<<<< HEAD:controllers/signup.js
 
     //add a function to check if user email already exists
 
     const user = await formData.create({
-=======
-    const user = await User.create({
->>>>>>> ec54c68128b065165186cca0bbf6f53d005b8095:controllers/signup.controller.js
       fname,
       lname,
       username,
       password: hashedPassword,
     });
 
-    res.status(200).json({ errorcode: 0, status: true, message: "User created succesfully", data: users });
+    res.status(200).json({ errorcode: 0, status: true, message: "User created succesfully"});
   } catch (err) {
     res.json({ error: err.message });
   }
