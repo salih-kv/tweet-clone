1d9fc7160255ed7a90e78e134a555bc3c87d0c78
linson_kw <linson.lorance@kilowott.com>
2023-08-25 23:12:31 +0530

    1-added function for token generation 2-changed code for error codes , removed returns and added token in Login async logic. 3-Added status code for succesful user creation in signup

--- controllers/signup.js
+++ controllers/signup.js
@@ -7,12 +7,16 @@ export const createUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    //add a function to check if user email already exists
+
     const user = await formData.create({
       fname,
       lname,
       email,
       password: hashedPassword,
     });
+
+    res.status(200).json({ errorcode: 0, status: true, message: "User created succesfully", data: users });
   } catch (err) {
     res.json({ error: err.message });
   }
