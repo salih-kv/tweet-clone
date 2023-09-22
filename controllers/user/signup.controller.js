fc1ad626032058ce3d009c1f8441a8774176d597
linson_kw <linson.lorance@kilowott.com>
2023-09-22 22:18:18 +0530

    User already exists error response created

--- controllers/user/signup.controller.js
+++ controllers/user/signup.controller.js
@@ -6,7 +6,6 @@ export const createUser = async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     //add a function to check if user email already exists
-
     const user = await Users.create({
       fname,
       lname,
@@ -19,6 +18,11 @@ export const createUser = async (req, res, next) => {
       message: "User created successfully",
     });
   } catch (err) {
-    next(err);
+    console.log(err,"error name")
+    if (err.name === 'MongoServerError' && err.code === 11000) {
+      next(new Error('User already exists'));
+  } else {
+    next(err);// ?
+  }
   }
 };
