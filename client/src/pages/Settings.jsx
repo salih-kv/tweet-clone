9194bbfed6c76c84eeaf2396e5c6198eb0152ed1
linson_kw <linson.lorance@kilowott.com>
2023-09-27 11:55:29 +0530

    setting navigation link active logic implemented

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,8 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
 import { BsChevronRight } from "react-icons/bs";
 import Header from "../components/Header.jsx";
+import { useEffect, useState } from "react";
 
 const Settings = () => {
+  
   return (
     <div className="dark:bg-primary-bg dark:text-white bg-lightPrimary">
       <div className="max-w-6xl m-auto">
@@ -23,16 +25,34 @@ const Settings = () => {
 };
 
 const SettingsNav = () => {
+ 
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+ 
+  useEffect(() => {
+    let linknum=settingsLinks.findIndex(link=>window.location.pathname==link.to);
+    setActiveTabIndex(linknum)
+  }, []); 
+  
   return (
     <div className="list-none flex flex-col gap-2 border-r dark:border-secondary-bg">
-      {settingsLinks.map((link) => (
+      {settingsLinks.map((link,idx) => (
         <div
           key={link.to}
-          className="border-b active:text-red-500 dark:border-secondary-bg border-b-gray-200 py-6 shadow-sm flex items-center justify-between"
+          
+          className={`border-b  dark:border-secondary-bg border-b-gray-200 py-6 shadow-sm flex items-center justify-between`}
+          
         >
-          <Link to={link.to}>{link.label}</Link>
+          <Link  to={link.to}  style={
+            idx === activeTabIndex
+              ? {color:"#1DA1F2"}
+              : {color:"black"}
+          }onClick={() => setActiveTabIndex(idx)}>{link.label}</Link>
           <span>
-            <BsChevronRight className="font-bold mr-4" />
+            <BsChevronRight style={
+            idx === activeTabIndex
+              ? {color:"#1DA1F2"}
+              : {color:"black"}
+          } className="font-bold mr-4" />
           </span>
         </div>
       ))}
